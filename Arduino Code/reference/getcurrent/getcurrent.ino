00aeebe46a9a0c95b3a760dfa0e05e4cbccd7b12
Pete Lewis <601236+lewispg228@users.noreply.github.com>
2020-02-27 14:24:11 -0700

    Slight adjustments to get current example

--- Arduino Code/reference/getcurrent/getcurrent.ino
+++ Arduino Code/reference/getcurrent/getcurrent.ino
@@ -5,19 +5,27 @@ Adafruit_INA219 ina219;
 
 long current_time;
 
+#include <Servo.h>
 
-void setup(void) 
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+float current_mA = 0;
+
+void setup(void)
 {
-  Serial.begin(115200);
+  Serial.begin(230400);
   while (!Serial) {
-      // will pause Zero, Leonardo, etc until serial console opens
-      delay(1);
+    // will pause Zero, Leonardo, etc until serial console opens
+    delay(1);
   }
 
   uint32_t currentFrequency;
-    
+
   Serial.println("Hello!");
-  
+
   // Initialize the INA219.
   // By default the initialization will use the largest range (32V, 2A).  However
   // you can call a setCalibration function to change this range (see comments).
@@ -29,23 +37,31 @@ void setup(void)
 
   Serial.println("Current(mA)");
   current_time = millis();
+
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(0);
+  delay(1000);
 }
 
-void loop(void) 
+void loop(void)
 {
-  float current_mA = 0;
 
+  myservo.write(180);
+
+while(1)
+{
   current_mA = ina219.getCurrent_mA();
 
-//  Serial.print(millis() - current_time );
-//  Serial.print(" ");
+  //  Serial.print(millis() - current_time );
+  //  Serial.print(" ");
   Serial.println(current_mA); //Serial.println(" mA");
 
   current_time = millis();
 
-  
-//  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
-//  Serial.println("");
 
-  delay(98);
+  //  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  //  Serial.println("");
+
+  //delay(1);
+}
 }
